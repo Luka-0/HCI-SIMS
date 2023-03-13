1f1a0812eaf7526395c146899f7cfe3b96698ca3
arsicIN48 <arsic.aleksandra83@gmail.com>
2023-03-13 13:11:32 +0100

    Renamed property - Location class

--- SIMS-Initial-Project-main/InitialProject/InitialProject/Model/Location.cs
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Model/Location.cs
@@ -10,6 +10,6 @@ namespace InitialProject.Model
     {
         public int Id { get; set; }
         public string City { get; set; }
-        public string State { get; set;  }
+        public string Contry { get; set;  }
     }
 }
