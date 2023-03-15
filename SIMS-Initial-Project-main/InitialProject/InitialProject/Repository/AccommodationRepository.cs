c9a9bce46377b60ba4b6331669c621e2b7bd46a1
LukaStjc <luka2001stajic@gmail.com>
2023-03-15 19:27:10 +0100

    Making a skelet for first two functionalities

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Repository/AccommodationRepository.cs
@@ -0,0 +1,143 @@
+﻿using InitialProject.Contexts;
+using InitialProject.Enumeration;
+using InitialProject.Migrations;
+using InitialProject.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace InitialProject.Repository
+{
+    class AccommodationRepository
+    {
+        public AccommodationRepository() { }
+
+        public static List<Accommodation> GetAll()
+        {
+            List<Accommodation> retVal = new();
+
+            using var db = new UserContext();
+            foreach (Accommodation accommodation in db.accommodation)
+            {
+                retVal.Add(accommodation);
+            }
+
+            if(retVal == null)  //TODO
+            {
+
+            }
+
+            return retVal;
+        }
+
+        public static List<Accommodation> GetByName(string name)
+        {
+            List<Accommodation> retVal = new();
+
+            using var db = new UserContext();
+            foreach(Accommodation accommodation in db.accommodation)
+            {
+                if (accommodation.Title.Contains(name))
+                {
+                    retVal.Add(accommodation);
+                }
+            }
+
+            if(retVal == null)  // TODO
+            {
+
+            }
+
+            return retVal;
+        }
+
+        public static List<Accommodation> GetBy(Location location)
+        {
+            List<Accommodation> retVal = new();
+
+            using var db = new UserContext();
+            foreach (Accommodation accommodation in db.accommodation)
+            {
+                if (accommodation.location.Equals(location))    //TODO: proveriti da li se radi sa equals ili ==
+                {
+                    retVal.Add(accommodation);
+                }
+            }
+
+            if (retVal == null)  // TODO
+            {
+
+            }
+
+            return retVal;
+        }
+
+        public static List<Accommodation> GetByType(AccommodationType accommodationType)
+        {
+            List<Accommodation> retVal = new();
+
+            using var db = new UserContext();
+            foreach (Accommodation accommodation in db.accommodation)
+            {
+                if (accommodation.Type == accommodationType)
+                {
+                    retVal.Add(accommodation);
+                }
+            }
+
+            if (retVal == null) //TODO
+            {
+
+            }
+
+            return retVal;
+        }
+
+        public static List<Accommodation> GetByGuestNumber(int guestNumber)
+        {
+            List<Accommodation> retVal = new();
+
+            using var db = new UserContext();
+            foreach(Accommodation accommodation in db.accommodation)
+            {
+                if(accommodation.GuestLimit >= guestNumber)
+                {
+                    retVal.Add(accommodation);
+                }
+            }
+
+            if (retVal == null) //TODO
+            {
+
+            }
+
+            return retVal;
+        }
+
+        public static List<Accommodation> GetByReservationDays(int reservationDays)
+        {
+            List<Accommodation> retVal = new();
+
+            using var db = new UserContext();
+            foreach(Accommodation accommodation in db.accommodation)
+            {
+                if(accommodation.MinimumReservationDays < reservationDays)
+                {
+                    retVal.Add(accommodation);
+                }
+            }
+
+            if(retVal == null)  //TODO
+            {
+
+            }
+
+            return retVal;
+        }
+    }
+}
