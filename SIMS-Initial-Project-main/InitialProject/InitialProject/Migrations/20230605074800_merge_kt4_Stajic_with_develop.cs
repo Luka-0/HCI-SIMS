2b75eb47191a17270a27f827f0797eb592a33cf2
LukaStjc <luka2001stajic@gmail.com>
2023-06-05 09:49:14 +0200

    Updated db after merging

---
+++ SIMS-Initial-Project-main/InitialProject/InitialProject/Migrations/20230605074800_merge_kt4_Stajic_with_develop.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InitialProject.Migrations
+{
+    public partial class merge_kt4_Stajic_with_develop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
